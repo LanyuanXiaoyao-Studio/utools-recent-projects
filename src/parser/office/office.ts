95c75679a6f7c6bb3ac18495477a560af83939be
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-08-19 11:15:48 +0800

    增加 Office win 适配

--- src/parser/office/office.ts
+++ src/parser/office/office.ts
@@ -6,12 +6,16 @@ import {
     ProjectItemImpl,
     SettingItem,
     ShellExecutor,
+    UToolsExecutor,
 } from '../../types'
 import {isEmpty, isNil, Url} from 'licia'
-import {parse} from 'path'
+import {join, parse} from 'path'
+import {execSync} from 'child_process'
 import plistParser = require('bplist-parser')
+import fs = require('fs')
 
 const OFFICE_MAC: string = 'office-mac'
+const OFFICE_WIN: string = 'office-win'
 
 const extensionIcon: (string) => string = extensionName => {
     switch (extensionName) {
@@ -30,7 +34,7 @@ const extensionIcon: (string) => string = extensionName => {
     }
 }
 
-export class OfficeMacProjectItemImpl extends ProjectItemImpl {
+export class OfficeProjectItemImpl extends ProjectItemImpl {
     datetime: number
 
     constructor(id: string, title: string, description: string, icon: string, searchKey: string, command: Executor, datetime: number) {
@@ -39,13 +43,13 @@ export class OfficeMacProjectItemImpl extends ProjectItemImpl {
     }
 }
 
-export class OfficeMacApplicationImpl extends ApplicationImpl<OfficeMacProjectItemImpl> {
+export class OfficeMacApplicationImpl extends ApplicationImpl<OfficeProjectItemImpl> {
     constructor(id: string, name: string, icon: string, configFilename: string, description: string = '') {
         super(`office-mac-${id}`, name, icon, OFFICE_MAC, [Platform.darwin], 'Office', configFilename, description, true)
     }
 
-    async generateProjectItems(): Promise<Array<OfficeMacProjectItemImpl>> {
-        let items: Array<OfficeMacProjectItemImpl> = []
+    async generateProjectItems(): Promise<Array<OfficeProjectItemImpl>> {
+        let items: Array<OfficeProjectItemImpl> = []
         let data = await plistParser.parseFile(this.config)
         if (!isNil(data) && !isEmpty(data)) {
             data = data[0]
@@ -96,8 +100,79 @@ export class OfficeMacApplicationImpl extends ApplicationImpl<OfficeMacProjectIt
     }
 }
 
-export const applications: Array<ApplicationImpl<OfficeMacProjectItemImpl>> = [
+export class OfficeWinApplicationImpl extends ApplicationImpl<OfficeProjectItemImpl> {
+    constructor() {
+        super(
+            `office-win`,
+            'Office 2019',
+            'icon/office.png',
+            OFFICE_WIN,
+            [Platform.win32],
+            'Office',
+            'Recent',
+            undefined,
+            true,
+        )
+    }
+
+    private recentPath: string = 'C:\\Users\\Administrator\\AppData\\Roaming\\Microsoft\\Office\\Recent'
+    private permitExtension: (string) => boolean = p => {
+        let parser = parse(p)
+        switch (parser.ext) {
+            case '.docx':
+            case '.xlsx':
+                return true
+            default:
+                return false
+        }
+    }
+    private generateCommand: (string) => string = link => `(New-Object -COM WScript.Shell).CreateShortcut('${link}').TargetPath;`
+
+    async generateProjectItems(): Promise<Array<OfficeProjectItemImpl>> {
+        let items: Array<OfficeProjectItemImpl> = []
+        let command = fs.readdirSync(this.recentPath)
+            .map(p => join(this.recentPath, p))
+            .filter(p => p.endsWith('LNK'))
+            .filter(p => this.permitExtension(p.replace(/\.LNK$/, '')))
+            .map(p => {
+                let stat = fs.lstatSync(p)
+                return {
+                    path: p,
+                    datetime: stat.mtimeMs,
+                }
+            })
+            .sort((p1, p2) => p2.datetime - p1.datetime)
+            .map(p => this.generateCommand(p.path))
+            .join('')
+        let result = execSync(`powershell.exe -command "${command}"`, { encoding: 'utf8' }).trim()
+        let paths = result.split(/\r?\n/)
+        paths.forEach(p => {
+            let parser = parse(p)
+            items.push({
+                id: '',
+                title: parser.name,
+                description: p,
+                icon: extensionIcon(parser.ext.replace(/\./g, '')),
+                searchKey: p,
+                command: new UToolsExecutor(p),
+                datetime: 0,
+            })
+        })
+        return items
+    }
+
+    generateSettingItems(nativeId: string): Array<SettingItem> {
+        return []
+    }
+
+    isFinishConfig(): ApplicationConfigState {
+        return ApplicationConfigState.done
+    }
+}
+
+export const applications: Array<ApplicationImpl<OfficeProjectItemImpl>> = [
     new OfficeMacApplicationImpl('word', 'Word', 'icon/office-word.png', 'com.microsoft.Word.securebookmarks.plist'),
     new OfficeMacApplicationImpl('excel', 'Excel', 'icon/office-excel.png', 'com.microsoft.Excel.securebookmarks.plist'),
     new OfficeMacApplicationImpl('powerpoint', 'PowerPoint', 'icon/office-powerpoint.png', 'com.microsoft.Powerpoint.securebookmarks.plist'),
+    new OfficeWinApplicationImpl(),
 ]
