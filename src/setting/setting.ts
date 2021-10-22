396902a7ab00b0e6cf41fe8754d6782f7fc985cd
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-10-22 13:16:04 +0800

    移除私人代码入口

--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -5,11 +5,6 @@ import {Context} from '../context'
 
 export class SettingUIArgsImpl implements Args<any> {
     enter(action: Action, callback: Callback<any>): void {
-        import('../inits')
-            .then(reports => {
-                reports.report(utools.getUser()?.nickname, action.code, utools.getNativeId(), platformFromUtools().toString())
-            })
-            .catch(error => console.log(error?.message ?? ''))
         utools.setExpendHeight(520)
 
         let context = Context.get()
