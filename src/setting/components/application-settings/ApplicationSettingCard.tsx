5e85b4c7a0d0315ade5f293da430598d4d68e6c4
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-11-21 11:21:08 +0800

    拆分插件设置和 chips

--- src/setting/components/application-settings/ApplicationSettingCard.tsx
+++ src/setting/components/application-settings/ApplicationSettingCard.tsx
@@ -1,66 +1,7 @@
 import Nano, {Component, Fragment, Img} from 'nano-jsx'
 import {Context} from '../../../Context'
 import {i18n, sentenceKey} from '../../../i18n'
-
-class CloudSyncChip extends Component {
-    override render() {
-        return (
-            <Fragment>
-                <span
-                    class="chip-wrapper tooltip tooltip-right"
-                    data-tooltip={i18n.t(sentenceKey.cloudDesc)}
-                >
-                    <span class="chip c-hand cloud">{i18n.t(sentenceKey.cloud)}</span>
-                </span>
-            </Fragment>
-        )
-    }
-}
-
-class NativeChip extends Component {
-    override render() {
-        return (
-            <Fragment>
-                <span
-                    class="chip-wrapper tooltip tooltip-right"
-                    data-tooltip={i18n.t(sentenceKey.localDesc)}
-                >
-                    <span class="chip c-hand native">{i18n.t(sentenceKey.local)}</span>
-                </span>
-            </Fragment>
-        )
-    }
-}
-
-class RebootNeededChip extends Component {
-    override render() {
-        return (
-            <Fragment>
-                <span
-                    class="chip-wrapper tooltip tooltip-right"
-                    data-tooltip={i18n.t(sentenceKey.needRebootDesc)}
-                >
-                    <span class="chip c-hand need-reboot">{i18n.t(sentenceKey.needReboot)}</span>
-                </span>
-            </Fragment>
-        )
-    }
-}
-
-class TestChip extends Component {
-    override render() {
-        return (
-            <Fragment>
-                <span
-                    class="chip-wrapper tooltip tooltip-right"
-                    data-tooltip={i18n.t(sentenceKey.settingBetaDesc)}
-                >
-                    <span class="chip c-hand test">{i18n.t(sentenceKey.beta)}</span>
-                </span>
-            </Fragment>
-        )
-    }
-}
+import {CloudSyncChip, NativeChip, RebootNeededChip, TestChip} from './Chips'
 
 export interface ApplicationSettingCardProps {
     context: Context
---
+++ src/setting/components/application-settings/Chips.tsx
@@ -0,0 +1,61 @@
+import Nano, {Component, Fragment} from 'nano-jsx'
+import {i18n, sentenceKey} from '../../../i18n'
+
+export class CloudSyncChip extends Component {
+    override render() {
+        return (
+            <Fragment>
+                <span
+                    class="chip-wrapper tooltip tooltip-right"
+                    data-tooltip={i18n.t(sentenceKey.cloudDesc)}
+                >
+                    <span class="chip c-hand cloud">{i18n.t(sentenceKey.cloud)}</span>
+                </span>
+            </Fragment>
+        )
+    }
+}
+export class NativeChip extends Component {
+    override render() {
+        return (
+            <Fragment>
+                <span
+                    class="chip-wrapper tooltip tooltip-right"
+                    data-tooltip={i18n.t(sentenceKey.localDesc)}
+                >
+                    <span class="chip c-hand native">{i18n.t(sentenceKey.local)}</span>
+                </span>
+            </Fragment>
+        )
+    }
+}
+
+export class RebootNeededChip extends Component {
+    override render() {
+        return (
+            <Fragment>
+                <span
+                    class="chip-wrapper tooltip tooltip-right"
+                    data-tooltip={i18n.t(sentenceKey.needRebootDesc)}
+                >
+                    <span class="chip c-hand need-reboot">{i18n.t(sentenceKey.needReboot)}</span>
+                </span>
+            </Fragment>
+        )
+    }
+}
+
+export class TestChip extends Component {
+    override render() {
+        return (
+            <Fragment>
+                <span
+                    class="chip-wrapper tooltip tooltip-right"
+                    data-tooltip={i18n.t(sentenceKey.settingBetaDesc)}
+                >
+                    <span class="chip c-hand test">{i18n.t(sentenceKey.beta)}</span>
+                </span>
+            </Fragment>
+        )
+    }
+}
